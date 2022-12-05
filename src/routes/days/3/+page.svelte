ce955bed4eba7b169fb6b9ab50dc795a294385f6
Matt Schwartz <19769905+schwartzmj@users.noreply.github.com>
2022-12-05 12:11:56 -0700

    day 3 page

--- src/routes/days/3/+page.svelte
+++ src/routes/days/3/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 	import Day3Part1 from '../../../days/3/part-1';
 	import Day3Part2 from '../../../days/3';
+	import Day3Part1Raw from '../../../days/3/part-1.ts?raw';
+	import Day3Part2Raw from '../../../days/3/index.ts?raw';
+	import Syntax from '$components/Syntax.svelte';
 </script>
 
 <h1>Day 3</h1>
 <button on:click={() => Day3Part1()}>Run Part 1</button>
 <button on:click={() => Day3Part2()}>Run Part 2</button>
+<details>
+	<summary> Part 1 </summary>
+	<Syntax language="typescript" code={Day3Part1Raw} />
+</details>
+<details>
+	<summary> Part 2 </summary>
+	<Syntax language="typescript" code={Day3Part2Raw} />
+</details>
